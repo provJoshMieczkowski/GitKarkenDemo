3c8256a556408cd50bd30c7d38c40be1ccdf6ef4
Josh J Mieczkowski <josh.mieczkowski@providence.org>
2020-04-07 09:31:16 -0700

    init commit

---
+++ app/src/main/java/org/dexcare/gitkrakendemo/MainActivity.kt
@@ -0,0 +1,25 @@
+package org.dexcare.gitkrakendemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        doingSomeWork()
+    }
+
+    private fun doingSomeWork(){
+        (0..100).map {
+            it * 2
+        }.map {
+            it.toString()
+        }.forEach {
+            val message = "current value: $it"
+            Log.wtf("TEST", message)
+        }
+    }
+}
